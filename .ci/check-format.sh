e523a43cfcced7be25f1a8aef2751c616812d2ad
Jim Huang <jserv.tw@gmail.com>
2023-07-08 05:39:04 +0000

    CI: Decouple format checks

--- .ci/check-format.sh
+++ .ci/check-format.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-SOURCES=$(find $(git rev-parse --show-toplevel) | egrep "\.(c|h)\$")
+SOURCES=$(find $(git rev-parse --show-toplevel) | egrep "\.(c|cxx|cpp|h|hpp)\$")
 
 set -x
 
-exit $(clang-format --output-replacements-xml ${SOURCES} | egrep -c "</replacement>")
+for file in ${SOURCES};
+do
+    clang-format-12 ${file} > expected-format
+    diff -u -p --label="${file}" --label="expected coding style" ${file} expected-format
+done
+exit $(clang-format-12 --output-replacements-xml ${SOURCES} | egrep -c "</replacement>")
