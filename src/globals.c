4152840b29268614b63b34c569d805f78e64cad9
Egil Moeller <em@emrld.no>
2024-01-04 23:09:39 +0100

    Bugfix

--- src/globals.c
+++ src/globals.c
@@ -637,7 +637,7 @@ void error(char *msg)
 {
     char src[100];
     strncpy(src, SOURCE+source_idx, 99);
-    src[99] = '\0';
+    src[99] = 0;
 
     /* TODO: figure out the corresponding C source and report line number */
     printf("Error %s at source location %d (%s)\n", msg, source_idx, src);
