fb9cf108b7f319c959145e2c71a8e6b1003820ad
Vacantron Chen <e24081030@gs.ncku.edu.tw>
2024-03-18 00:22:11 +0800

    Fix unsorted slots in snapshots

--- tests/update-snapshots.sh
+++ tests/update-snapshots.sh
@@ -13,7 +13,7 @@ function update_snapshot() {
     $SHECC --dump-ir -o $temp_exe $source &>/dev/null
     dot -Tdot_json -o $temp_json CFG.dot
     sed -i -E "/0x[0-9a-f]+/d" $temp_json
-    jq -c . $temp_json > $dest
+    jq -S -c . $temp_json > $dest
 }
 
 for file in tests/*.c; do
