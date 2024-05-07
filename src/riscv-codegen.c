sysprog21/shecc

commit 7f328f33a97d21096d36340f773f16cffc714cce

Apply editorial changes

src/riscv-codegen.c
@@ -98,34 +98,35 @@ void cfg_flatten()
     GLOBAL_FUNC.fn->bbs->elf_offset = elf_offset;
 
     for (ph2_ir_t *ph2_ir = GLOBAL_FUNC.fn->bbs->ph2_ir_list.head; ph2_ir;
-         ph2_ir = ph2_ir->next)
+         ph2_ir = ph2_ir->next) {
         update_elf_offset(ph2_ir);
+    }
 
-    /* prepare `argc` and `argv`, then proceed to `main` function */
+    /* prepare 'argc' and 'argv', then proceed to 'main' function */
     elf_offset += 24;
 
     for (fn_t *fn = FUNC_LIST.head; fn; fn = fn->next) {
-        ph2_ir_t *flatten_ir;
-
         /* reserve stack */
-        flatten_ir = add_ph2_ir(OP_define);
+        ph2_ir_t *fflatten_ir = add_ph2_ir(OP_define);
         flatten_ir->src0 = fn->func->stack_size;
 
         for (basic_block_t *bb = fn->bbs; bb; bb = bb->rpo_next) {
             bb->elf_offset = elf_offset;
 
-            if (bb == fn->bbs)
+            if (bb == fn->bbs) {
                 /* save ra, sp */
                 elf_offset += 16;
+            }
 
             for (ph2_ir_t *insn = bb->ph2_ir_list.head; insn;
                  insn = insn->next) {
                 flatten_ir = add_ph2_ir(OP_generic);
                 memcpy(flatten_ir, insn, sizeof(ph2_ir_t));
 
-                if (insn->op == OP_return)
+                if (insn->op == OP_return) {
                     /* restore sp */
                     flatten_ir->src1 = bb->belong_to->func->stack_size;
+                }
 
                 update_elf_offset(flatten_ir);
             }
@@ -388,7 +389,7 @@ void code_generate()
          ph2_ir = ph2_ir->next)
         emit_ph2_ir(ph2_ir);
 
-    /* prepare `argc` and `argv`, then proceed to `main` function */
+    /* prepare 'argc' and 'argv', then proceed to 'main' function */
     emit(__lui(__t0, rv_hi(GLOBAL_FUNC.stack_size)));
     emit(__addi(__t0, __t0, rv_lo(GLOBAL_FUNC.stack_size)));
     emit(__add(__t0, __gp, __t0));

src/arm-codegen.c
@@ -13,10 +13,9 @@ void update_elf_offset(ph2_ir_t *ph2_ir)
 {
     switch (ph2_ir->op) {
     case OP_load_constant:
-        /* ARMv7 uses 12 bits to encode immediate value, but the
-         * higher 4 bits are for rotation. See A5.2.4 "Modified
-         * immediate constants in ARM instructions" in ARMv7-A
-         * manual.
+        /* ARMv7 uses 12 bits to encode immediate value, but the higher 4 bits
+         * are for rotation. See A5.2.4 "Modified immediate constants in ARM
+         * instructions" in ARMv7-A manual.
          */
         if (ph2_ir->src0 < 0)
             elf_offset += 12;
@@ -27,10 +26,9 @@ void update_elf_offset(ph2_ir_t *ph2_ir)
         return;
     case OP_address_of:
     case OP_global_address_of:
-        /* ARMv7 uses 12 bits to encode immediate value, but the
-         * higher 4 bits are for rotation. See A5.2.4 "Modified
-         * immediate constants in ARM instructions" in ARMv7-A
-         * manual.
+        /* ARMv7 uses 12 bits to encode immediate value, but the higher 4 bits
+         * are for rotation. See A5.2.4 "Modified immediate constants in ARM
+         * instructions" in ARMv7-A manual.
          */
         if (ph2_ir->src0 > 255)
             elf_offset += 12;
@@ -45,8 +43,8 @@ void update_elf_offset(ph2_ir_t *ph2_ir)
         return;
     case OP_load:
     case OP_global_load:
-        /* ARMv7 straight uses 12 bits to encode the offset of
-         * load instruction (no rotation).
+        /* ARMv7 straight uses 12 bits to encode the offset of load instruction
+         * (no rotation).
          */
         if (ph2_ir->src0 > 4095)
             elf_offset += 16;
@@ -57,8 +55,8 @@ void update_elf_offset(ph2_ir_t *ph2_ir)
         return;
     case OP_store:
     case OP_global_store:
-        /* ARMv7 straight uses 12 bits to encode the offset of
-         * store instruction (no rotation).
+        /* ARMv7 straight uses 12 bits to encode the offset of store instruction
+         * (no rotation).
          */
         if (ph2_ir->src1 > 4095)
             elf_offset += 16;
@@ -126,10 +124,11 @@ void cfg_flatten()
     GLOBAL_FUNC.fn->bbs->elf_offset = elf_offset;
 
     for (ph2_ir_t *ph2_ir = GLOBAL_FUNC.fn->bbs->ph2_ir_list.head; ph2_ir;
-         ph2_ir = ph2_ir->next)
+         ph2_ir = ph2_ir->next) {
         update_elf_offset(ph2_ir);
+    }
 
-    /* prepare `argc` and `argv`, then proceed to `main` function */
+    /* prepare 'argc' and 'argv', then proceed to 'main' function */
     elf_offset += 24;
 
     for (fn_t *fn = FUNC_LIST.head; fn; fn = fn->next) {
@@ -142,21 +141,23 @@ void cfg_flatten()
         for (basic_block_t *bb = fn->bbs; bb; bb = bb->rpo_next) {
             bb->elf_offset = elf_offset;
 
-            if (bb == fn->bbs)
+            if (bb == fn->bbs) {
                 /* save ra, sp */
                 elf_offset += 16;
+            }
 
             for (ph2_ir_t *insn = bb->ph2_ir_list.head; insn;
                  insn = insn->next) {
                 flatten_ir = add_ph2_ir(OP_generic);
                 memcpy(flatten_ir, insn, sizeof(ph2_ir_t));
 
-                if (insn->op == OP_return)
+                if (insn->op == OP_return) {
                     /* restore sp */
                     flatten_ir->src1 = bb->belong_to->func->stack_size;
+                }
 
                 if (insn->op == OP_branch) {
-                    /* In SSA, we index `else_bb` first, and then `then_bb` */
+                    /* In SSA, we index 'else_bb' first, and then 'then_bb' */
                     if (insn->else_bb != bb->rpo_next)
                         flatten_ir->is_branch_detached = 1;
                 }
@@ -415,7 +416,7 @@ void code_generate()
          ph2_ir = ph2_ir->next)
         emit_ph2_ir(ph2_ir);
 
-    /* prepare `argc` and `argv`, then proceed to `main` function */
+    /* prepare 'argc' and 'argv', then proceed to 'main' function */
     emit(__movw(__AL, __r8, GLOBAL_FUNC.stack_size));
     emit(__movt(__AL, __r8, GLOBAL_FUNC.stack_size));
     emit(__add_r(__AL, __r8, __r12, __r8));

src/defs.h
@@ -56,7 +56,7 @@
 #else
 /* suppress GCC/Clang warnings */
 #define UNUSED(x) (void) (x)
-/* configure host data model when using `memcpy()` */
+/* configure host data model when using 'memcpy'. */
 #define HOST_PTR_SIZE __SIZEOF_POINTER__
 #endif
 

src/globals.c
@@ -76,9 +76,9 @@ char *elf_section;
  * insert_trie() - Inserts a new element into the trie structure.
  * @trie: A pointer to the trie where the name will be inserted.
  * @name: The name to be inserted into the trie.
- * @funcs_index: The index of the pointer to the func_t. The index is
- *               recorded in a 1-indexed format. Because the first element of
- *               `FUNCS` has been reserved, there's no need to shift it.
+ * @funcs_index: The index of the pointer to the func_t. The index is recorded
+ *     in a 1-indexed format. Because the first element of 'FUNCS' has been
+ *     reserved, there is no need to shift it.
  * Return: The index of the pointer to the func_t.
  *
  * If the function has been inserted, the return value is the index of the
@@ -155,8 +155,7 @@ type_t *find_type(char *type_name, int flag)
             if (flag == 2)
                 continue;
             if (!strcmp(TYPES[i].type_name, type_name)) {
-                /*
-                 * If it is a forwardly declared alias of a structure, return
+                /* If it is a forwardly declared alias of a structure, return
                  * the base structure type.
                  */
                 if (TYPES[i].base_type == TYPE_typedef && TYPES[i].size == 0)
@@ -305,7 +304,7 @@ func_t *add_func(char *name)
         strcpy(fn->return_def.var_name, name);
     }
     fn = &FUNCS[index];
-    fn->stack_size = 4; /*starting point of stack */
+    fn->stack_size = 4; /* starting point of stack */
     return fn;
 }
 
@@ -347,8 +346,7 @@ func_t *find_func(char func_name[])
 
 var_t *find_member(char token[], type_t *type)
 {
-    /*
-     * If it is a forwardly declared alias of a structure, switch to the base
+    /* If it is a forwardly declared alias of a structure, switch to the base
      * structure type.
      */
     if (type->size == 0)
@@ -419,7 +417,7 @@ int size_var(var_t *var)
     return size;
 }
 
-/* TODO: Integrate with `func_t` */
+/* TODO: Integrate with 'func_t' */
 fn_t *add_fn()
 {
     fn_t *n = calloc(1, sizeof(fn_t));
@@ -434,7 +432,7 @@ fn_t *add_fn()
     return n;
 }
 
-/* Create a basic block and set the scope of variables to `parent` block */
+/* Create a basic block and set the scope of variables to 'parent' block */
 basic_block_t *bb_create(block_t *parent)
 {
     basic_block_t *bb = calloc(1, sizeof(basic_block_t));
@@ -623,7 +621,8 @@ void global_release()
 void error(char *msg)
 {
     /* Construct error source diagnostics, enabling precise identification of
-     * syntax and logic issues within the code. */
+     * syntax and logic issues within the code.
+     */
     int offset, start_idx, i = 0;
     char diagnostic[512 /* MAX_LINE_LEN * 2 */];
 
@@ -645,7 +644,8 @@ void error(char *msg)
     strcpy(diagnostic + i, "^ Error occurs here");
 
     /* TODO: figure out the corresponding C source file path and report line
-     * number */
+     * number.
+     */
     printf("Error %s at source location %d\n%s\n", msg, source_idx, diagnostic);
     abort();
 }

src/lexer.c
@@ -174,7 +174,7 @@ char peek_char(int offset)
     return SOURCE[source_idx + offset];
 }
 
-/* Lex next token and returns its token type. Parameter `aliasing` is used for
+/* Lex next token and returns its token type. Parameter 'aliasing' is used for
  * disable preprocessor aliasing on identifier tokens.
  */
 token_t lex_token_internal(int aliasing)
@@ -549,7 +549,8 @@ token_t lex_token_internal(int aliasing)
 }
 
 /* Lex next token and returns its token type. To disable aliasing on next
- * token, use `lex_token_internal`. */
+ * token, use 'lex_token_internal'.
+ */
 token_t lex_token()
 {
     return lex_token_internal(1);
@@ -576,16 +577,16 @@ int lex_accept_internal(token_t token, int aliasing)
     return 0;
 }
 
-/* Accepts next token if token types are matched. To disable aliasing
- * on next token, use `lex_accept_internal`.
+/* Accepts next token if token types are matched. To disable aliasing on next
+ * token, use 'lex_accept_internal'.
  */
 int lex_accept(token_t token)
 {
     return lex_accept_internal(token, 1);
 }
 
-/* Peeks next token and copy token's literal to value if token types
- * are matched.
+/* Peeks next token and copy token's literal to value if token types are
+ * matched.
  */
 int lex_peek(token_t token, char *value)
 {
@@ -598,8 +599,8 @@ int lex_peek(token_t token, char *value)
     return 0;
 }
 
-/* Strictly match next token with given token type and copy token's
- * literal to value.
+/* Strictly match next token with given token type and copy token's literal to
+ * value.
  */
 void lex_ident_internal(token_t token, char *value, int aliasing)
 {
@@ -609,9 +610,8 @@ void lex_ident_internal(token_t token, char *value, int aliasing)
     next_token = lex_token_internal(aliasing);
 }
 
-/* Strictly match next token with given token type and copy token's
- * literal to value. To disable aliasing on next token, use
- * `lex_ident_internal`.
+/* Strictly match next token with given token type and copy token's literal to
+ * value. To disable aliasing on next token, use 'lex_ident_internal'.
  */
 void lex_ident(token_t token, char *value)
 {
@@ -626,8 +626,8 @@ void lex_expect_internal(token_t token, int aliasing)
     next_token = lex_token_internal(aliasing);
 }
 
-/* Strictly match next token with given token type. To disable aliasing
- * on next token, use `lex_expect_internal`.
+/* Strictly match next token with given token type. To disable aliasing on next
+ * token, use 'lex_expect_internal'.
  */
 void lex_expect(token_t token)
 {

src/main.c
@@ -36,7 +36,7 @@
 /* Peephole optimization */
 #include "peephole.c"
 
-/* Machine code generation. support ARMv7-A and RISC-V32I */
+/* Machine code generation. support ARMv7-A and RV32I */
 #include "codegen.c"
 
 /* inlined libc */

commit a1d1ddc57409dd6e5dd73233d76ecc2ab019a0c1

Fix compilation errors when ARCH=riscv
When ARCH=riscv, the following compilation errors occur:

In file included from src/main.c:41:
src/codegen.c: In function ‘cfg_flatten’:
src/codegen.c:111:9: error: ‘flatten_ir’ undeclared (first use in this function); did you mean ‘fflatten_ir’?
  111 |         flatten_ir->src0 = fn->func->stack_size;
      |         ^~~~~~~~~~
      |         fflatten_ir
src/codegen.c:111:9: note: each undeclared identifier is reported only once for each function it appears in
src/codegen.c:110:19: warning: unused variable ‘fflatten_ir’ [-Wunused-variable]
  110 |         ph2_ir_t *fflatten_ir = add_ph2_ir(OP_define);
      |                   ^~~~~~~~~~~

Fix the erroneous variable name 'fflatten_ir' to 'flatten_ir' to avoid
compilation errors.

Fixes: 7f328f33a97d ("Apply editorial changes")

src/riscv-codegen.c
@@ -107,7 +107,7 @@ void cfg_flatten()
 
     for (fn_t *fn = FUNC_LIST.head; fn; fn = fn->next) {
         /* reserve stack */
-        ph2_ir_t *fflatten_ir = add_ph2_ir(OP_define);
+        ph2_ir_t *flatten_ir = add_ph2_ir(OP_define);
         flatten_ir->src0 = fn->func->stack_size;
 
         for (basic_block_t *bb = fn->bbs; bb; bb = bb->rpo_next) {