bcb515fa2c77b5cd0fc53586221d37e45a7ebb9e
Yu-En Hsiao <drxiao6399@gmail.com>
2025-09-17 21:40:21 +0800

    Detect the sysroot path of the ARM GNU toolchain automatically
    Originally, the default path of ld-linux.so is set to a specific path
    for Arm architecture. However, Considering that developers may manually
    install the ARM GNU toolchain, the sysroot path may not match the
    default path.
    
    This commit improves arm.mk to use a more convenient method to
    automatically detect the correct sysroot path.

--- mk/arm.mk
+++ mk/arm.mk
@@ -13,4 +13,46 @@ ARCH_DEFS = \
     \#define R_ARCH_JUMP_SLOT 0x16\n$\
     \#define MAX_ARGS_IN_REG 4\n$\
     "
-RUNNER_LD_PREFIX=-L /usr/arm-linux-gnueabihf/
+
+# Find the sysroot of the ARM GNU toolchain if using dynamic linking.
+#
+# Since developers may install the toolchain manually instead of
+# using a package manager such as apt, we cannot assume that the
+# path of ld-linux is always "/usr/arm-linux-gnueabihf".
+#
+# Therefore, the following process first locates find the correct
+# sysroot of the toolchain, and then generate the ELF interpreter
+# prefix for later use.
+ifneq ($(HOST_ARCH),$(ARCH_NAME))
+    ifeq ($(DYNLINK),1)
+        CROSS_COMPILE = arm-none-linux-gnueabihf-
+        ARM_CC = $(CROSS_COMPILE)gcc
+        ARM_CC := $(shell which $(ARM_CC))
+        ifndef ARM_CC
+            CROSS_COMPILE = arm-linux-gnueabihf-
+            ARM_CC = $(CROSS_COMPILE)gcc
+            ARM_CC := $(shell which $(ARM_CC))
+            ifndef ARM_CC
+                $(error "Unable to find ARM GNU toolchain.")
+            endif
+        endif
+
+        LD_LINUX_PATH := $(shell cd $(shell $(ARM_CC) --print-sysroot) 2>/dev/null && pwd)
+        ifeq ("$(LD_LINUX_PATH)","/")
+            LD_LINUX_PATH := $(shell dirname "$(shell which $(ARM_CC))")/..
+            LD_LINUX_PATH := $(shell cd $(LD_LINUX_PATH) 2>/dev/null && pwd)
+            LD_LINUX_PATH := $(LD_LINUX_PATH)/$(shell echo $(CROSS_COMPILE) | sed s'/.$$//')/libc
+            LD_LINUX_PATH := $(shell cd $(LD_LINUX_PATH) 2>/dev/null && pwd)
+            ifndef LD_LINUX_PATH
+                LD_LINUX_PATH = /usr/$(shell echo $(CROSS_COMPILE) | sed s'/.$$//')
+                LD_LINUX_PATH := $(shell cd $(LD_LINUX_PATH) 2>/dev/null && pwd)
+            endif
+        endif
+
+        ifndef LD_LINUX_PATH
+            $(error "Dynamic linking mode requires ld-linux.so")
+        endif
+
+        RUNNER_LD_PREFIX = -L $(LD_LINUX_PATH)
+    endif
+endif
